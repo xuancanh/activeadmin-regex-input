8338a1ae86679db9ff1973e3996f57cce4e1f3ab
Canh Nguyen <nxcanh@fossil.com>
2018-03-14 10:47:48 +0700

    Change html output to match with active admin default style

--- app/assets/javascript/active_admin/regex_input.js
+++ app/assets/javascript/active_admin/regex_input.js
@@ -1,11 +1,33 @@
 //= require webui-popover
 
-function initRegexInput(inputs) {
+// https://css-tricks.com/snippets/javascript/get-url-variables/
+// Get url query params
+function getQueryVariable(key)
+{
+  const query = decodeURI(window.location.search.substring(1));
+  let vars = query.split("&");
+  for (let i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] === key){
+      // Replace + in url with space, might make the previous data became wrong
+      return decodeURIComponent(pair[1].replace('+', ' '));
+    }
+  }
+  return null;
+}
+
+// Init all reqex input
+function initRegexInputs(inputs) {
   for (let item of inputs) {
     let input = $(item);
     const regex = new RegExp(input.data('regex'));
     const example = input.data('example');
 
+    // Reassign input value with query param from url
+    const value = getQueryVariable(input.attr('name'));
+    if (value) {
+      input.val(value);
+    }
     // Create popover
     input.webuiPopover({
       title: 'Wrong input format',
@@ -13,20 +35,20 @@ function initRegexInput(inputs) {
       trigger: 'manual'
     });
 
-    // Show popover if input doesn't match regex
-    input.on('change', event => {
+    // Show popover and change color if current input doesn't match regex
+    input.on('keyup', event => {
       console.log(input.val());
       if (!regex.test(input.val())) {
         input.css('border-color', 'red');
         input.webuiPopover('show');
       } else {
         input.webuiPopover('hide');
-        input.css('border-color', 'black');
+        input.css('border-color', '#c9d0d6');
       }
     });
   }
 }
 
 $(document).ready(() => {
-  initRegexInput($('.regex-input'));
+  initRegexInputs($('.regex-input'));
 });
--- lib/formtastic/regex-input.rb
+++ lib/formtastic/regex-input.rb
@@ -3,10 +3,28 @@ module Formtastic
 
     class RegexInput < Formtastic::Inputs::StringInput
       def input_html_options
+
         {
             class: 'regex-input',
-            data: {regex: options[:regex]},
-        }.merge(super)
+            data: {
+                regex: options[:regex],
+                example: options[:example]
+            },
+            placeholder_text: options[:example]
+        }
+      end
+
+      # Overwrite default li tag
+      def input_wrapping(&block)
+        template.content_tag(:div,
+                            [template.capture(&block), error_html, hint_html].join("\n").html_safe,
+                            wrapper_html_options
+        )
+      end
+
+      # Overwrite wrapper html by the active admin class
+      def wrapper_html_options
+        super.merge(:class => 'filter_form_field filter_string')
       end
     end
 
